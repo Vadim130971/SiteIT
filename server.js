bcd6bdea83a6bdcac860d5c09c1cb6dcf8ebb5d7
Vadim130971 <interprog@ukr.net>
2019-02-18 16:27:12 +0200

    two exchange

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http'); //Подключаем модуль http
 const port = 3000;
 const server = http.createServer((req, res) => { //Создаем сервер
-  res.end('For all facken users!'); //Если кто то подключился отвечаем
+  res.end('For all facken users git hub !'); //Если кто то подключился отвечаем
   console.log(req.method, req.url);
 }).listen(3000); //Слушаем порт 3000
 console.log('Сервер работает на порте ' + port); 
