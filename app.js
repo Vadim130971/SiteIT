993b46dc53dcf692d7d1ae38ba73f67bd91e6781
Vadim130971 <interprog@ukr.net>
2019-02-19 17:45:33 +0200

    js script one

---
+++ app.js
@@ -0,0 +1,7 @@
+            document.write("<H1>");
+            document.write("1 + 1 = " , 1 + 1 );
+            document.write("</H1>");
+
+            document.write("<pre>");
+            document.write("   hell    git users  ");
+            document.write("</pre>");
